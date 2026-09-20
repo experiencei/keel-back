dd154d6fe5cd850bca8dac71a84700c5bb6958f0
experiencei <ayelojahighbee01@gmail.com>
2026-09-21 00:38:04 +0100

    adjustments needed

--- src/server.js
+++ src/server.js
@@ -18,7 +18,11 @@ import { accountRouter } from './routes/account.js';
 
 const app = express();
 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({
+  origin: ['https://keel-rose-ten.vercel.app/', 'http://localhost:3000'],
+  credentials: true,
+}));
+
 app.use(morgan('dev'));
 
 app.use('/api/webhooks', express.raw({ type: 'application/json' }), webhooksRouter);
